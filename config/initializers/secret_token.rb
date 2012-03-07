5b47ad09079769bbb5aead5d7b5252c00e01ab96
Zakri Kneebone <i1abnrk@yahoo.com>
2012-03-07 16:09:35 -0600

    rails standard setup with mysql and closure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supertaggerfm::Application.config.secret_token = 'd480372b15fb84ff9a643ad8c24cd7f545a217a8536175a5c5a41f415d96a7278fdf2de62735f9b09e25cb7bd3ea54ff1ae7b45b518259e7ef9646884171fa71'
